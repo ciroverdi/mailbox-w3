4c897d00e7a3bb15aa370806c9ab17f96eb1d9f1
Ciro Verdi <ciroverdi@gmail.com>
2015-10-22 21:20:28 -0200

    Creating Pan Gesture Recognizer

--- mailbox-w3/MailboxViewController.swift
+++ mailbox-w3/MailboxViewController.swift
@@ -8,28 +8,41 @@
 
 import UIKit
 
-class MailboxViewController: UIViewController {
+class MailboxViewController: UIViewController, UIGestureRecognizerDelegate, UIScrollViewDelegate {
 
+    @IBOutlet weak var messageView: UIImageView!
+    @IBOutlet weak var feedView: UIImageView!
+    @IBOutlet weak var feedScrollView: UIScrollView!
+    @IBOutlet var panRecognizer: UIPanGestureRecognizer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        feedScrollView.delegate = self
+        feedScrollView.contentSize = feedView.image!.size
+        
+        panRecognizer = UIPanGestureRecognizer(target: self, action: "onPan:")
+        messageView.addGestureRecognizer(panRecognizer)
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func onPan(sender: UIPanGestureRecognizer) {
+        var point = sender.locationInView(messageView)
+        var translation = sender.translationInView(messageView)
+        var velocity = sender.velocityInView(messageView)
+        
+        if panRecognizer.state == UIGestureRecognizerState.Began {
+            print("Gesture began at: \(point)")
+        } else if panRecognizer.state == UIGestureRecognizerState.Changed {
+            print("Gesture changed at: \(point)")
+        } else if panRecognizer.state == UIGestureRecognizerState.Ended {
+            print("Gesture ended at: \(point)")
+        }
+        
     }
-    */
-
+    
 }
